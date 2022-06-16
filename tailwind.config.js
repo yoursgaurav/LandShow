e4e64fcf0ba67978653923acf23b6ee301ea0f46
[Gaurav Shukla] <[yoursgaurav@yahoo.com]>
2022-06-16 19:21:44 +0530

    INitial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif',],
+    },
+    extend: {
+      colors: {
+        primary: '#7538A8',
+        seconadry: '#F5F5F5',
+      }
+    },
+  },
+  plugins: [],
+}
