37186f96868b5174535fef5be692fa21c4c7011d
[Gaurav Shukla] <[yoursgaurav@yahoo.com]>
2022-06-19 07:29:46 +0530

    Added dark mode functionality and improved code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-let togglebutton = document.querySelector("#toggleButton");
+// JavaScript for toggling navigation menu 
+
+let menuToggler = document.querySelector("#menu-toggler");
 let navMenu = document.querySelector("#nav-menu");
 
-togglebutton.addEventListener("click", toggleMenu);
+menuToggler.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-
-    console.log("Hello World!");
-
     navMenu.classList.toggle("hidden");
+}
+
+
+// JavaScript for changing mode 
+
+let modeToggler = document.querySelector("#mode-toggler");
+let page = document.querySelector("#mode");
+
+modeToggler.addEventListener("click", toggleMode);
+
+function toggleMode() {
+    page.classList.toggle("dark");
 }
